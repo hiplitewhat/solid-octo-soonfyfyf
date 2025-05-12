85a6c7183af3752f6260535a00370b3cfb1b7e66
hiplitewhat <hiplitewhat@gmail.com>
2025-05-13 01:36:26 +0800

    Update worker.ts

--- worker.ts
+++ worker.ts
@@ -1,6 +1,7 @@
 export default {
   async fetch(request, env, ctx) {
-    const url = "https://foil-interesting-recess.glitch.me/health";
+    const url = ["https://foil-interesting-recess.glitch.me/health",
+                 ""];
 
     try {
       const start = Date.now();
